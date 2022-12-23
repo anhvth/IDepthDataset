b3595ab3e819933ab76f7a70a85d4f4d7f5ba9e7
anh vo <anhvth.226@gmail.com>
2022-12-23 11:11:46 +0700

    update

--- dms.sh
+++ dms.sh
@@ -1,9 +1,7 @@
 curdir="/Users/bi/gitprojects/IDepthDataset/"
-# Replace /Users/bi with /home/anhvth8 
 new_dir="/home/anhvth8/gitprojects/IDepthDataset/"
-
 echo sync from $curdir to dms:$new_dir
-# CHeck if first args is pull
+
 if [ "$1" = "pull" ];
   then
     # Pull from dms
@@ -16,15 +14,12 @@ elif [ "$1" = "push" ];
     cmd="rsync -avz -e ssh --exclude-from 'exclude.txt' $curdir anhvth8@dms:$new_dir"
     echo $cmd
     eval $cmd
-elif [ "$1" = "run" ];
-  then
-    ./dms.sh push
-    python_cmd="/home/anhvth8/.conda/envs/swin/bin/python ${@:2}"
-    echo $python_cmd
-
-    cmd="ssh dms -t \"cd ${new_dir} && ${python_cmd}\""
-    echo $cmd
-    eval $cmd
 else
-    echo "Usage: dms.sh [pull|push|ssh]"
+    ./dms.sh push
+    clear
+    dms_cmd="${@:2}"
+    echo $dms_cmd
+
+    cmd="ssh dms -t \"cd ${new_dir} && ${dms_cmd}\""
+    eval $cmd
 fi
